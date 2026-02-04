b54f5b77251f38914506796d343ffeb7772194d0
vaishnavitodkar <112103489+vaishnavitodkar@users.noreply.github.com>
2026-02-05 02:59:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,39 @@
 import express from "express";
-import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
-
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-
 app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  origin: [
+    "http://localhost:3000",
+    "https://vercel-frontend-two-omega.vercel.app",
+    "https://vercel-frontend-iqg348njy-vaishnavitodkars-projects.vercel.app"
+  ],
+  credentials: true,
 }));
 
 app.use(express.json());
 app.use(morgan("dev"));
-
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).send({
-    success: true,
-    message: "Backend is running successfully 🚀"
-  });
+  res.send("Welcome to ecommerce app");
 });
-
 
 const PORT = process.env.PORT || 8000;
-
-
 app.listen(PORT, () => {
-  console.log(
-    `Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`
-      .bgCyan.white
-  );
+  console.log(`Server running on port ${PORT}`);
 });
-
