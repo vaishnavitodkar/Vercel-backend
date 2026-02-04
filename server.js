9c334db50099103fdc44ae6a53f41d597c2a3a5b
vaishnavitodkar <112103489+vaishnavitodkar@users.noreply.github.com>
2026-02-05 01:52:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://vercel-frontend-two-omega.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
